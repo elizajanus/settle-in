b2586b6b972988d6bab22497a464ddb12cc87105
Sophia Butterworth <sophiabutterworth@gmail.com>
2018-05-17 18:48:37 -0400

    adding image files for rentals, js files for rental api routes, js for reviews, rental model

---
+++ public/assets/js/rental.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var postTitle = $("");
+    var landlordName = $("");
+    // double check the buttons and the data that will be submitted here for reviews
+
+    $(document).on("submit", "#", handleReviewFormSubmit);
+    $(document).on("click", "#", handleDeleteButtonPress);
+
+    get
+})
