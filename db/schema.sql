dc7adc2e75c5f81f4c03a79f8d459f79831e4ca6
Eliza Janus <emj4em@virginia.edu>
2018-05-17 20:43:04 -0400

    fix database schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,41 +1,42 @@
+DROP DATABASE IF EXISTS settle_in;
 CREATE DATABASE settle_in;
 
-USE DATABASE settle_in;
+-- USE DATABASE settle_in;
 
-CREATE TABLE roommates
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(150) NOT NULL,
-    email varchar(150) NOT NULL,
-    age INTEGER(3) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    gender VARCHAR(150) NOT NULL,
-	PRIMARY KEY (id)
-);
+-- CREATE TABLE roommates
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	name varchar(150) NOT NULL,
+--     email varchar(150) NOT NULL,
+--     age INTEGER(3) NOT NULL,
+--     phone VARCHAR(20) NOT NULL,
+--     gender VARCHAR(150) NOT NULL,
+-- 	PRIMARY KEY (id)
+-- );
 
 
-CREATE TABLE survey
-(
-	id int NOT NULL AUTO_INCREMENT,
-	smoking BOOLEAN NOT NULL,
-    pets BOOLEAN NOT NULL,
-    ground BOOLEAN NOT NULL,
-    nightowl BOOLEAN NOT NULL,
-    clean BOOLEAN NOT NULL,
-    entertainer BOOLEAN NOT NULL,
-    religious BOOLEAN NOT NULL,
-    samegender BOOLEAN NOT NULL,
-    allergies VARCHAR(200),
-    relationship BOOLEAN NOT NULL,
-    roommaterelationship BOOLEAN NOT NULL,
-    groceries BOOLEAN NOT NULL,
-    bathroom BOOLEAN NOT NULL,
-    cooking BOOLEAN NOT NULL,
-    pricerange VARCHAR(200),
-    veg BOOLEAN NOT NULL,
-    personality VARCHAR(100),
-    kids BOOLEAN NOT NULL,
-    freetime VARCHAR(100),
-    wfh BOOLEAN NOT NULL,
-	PRIMARY KEY (id)
-);
+-- CREATE TABLE survey
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	smoking BOOLEAN NOT NULL,
+--     pets BOOLEAN NOT NULL,
+--     ground BOOLEAN NOT NULL,
+--     nightowl BOOLEAN NOT NULL,
+--     clean BOOLEAN NOT NULL,
+--     entertainer BOOLEAN NOT NULL,
+--     religious BOOLEAN NOT NULL,
+--     samegender BOOLEAN NOT NULL,
+--     allergies VARCHAR(200),
+--     relationship BOOLEAN NOT NULL,
+--     roommaterelationship BOOLEAN NOT NULL,
+--     groceries BOOLEAN NOT NULL,
+--     bathroom BOOLEAN NOT NULL,
+--     cooking BOOLEAN NOT NULL,
+--     pricerange VARCHAR(200),
+--     veg BOOLEAN NOT NULL,
+--     personality VARCHAR(100),
+--     kids BOOLEAN NOT NULL,
+--     freetime VARCHAR(100),
+--     wfh BOOLEAN NOT NULL,
+-- 	PRIMARY KEY (id)
+-- );
