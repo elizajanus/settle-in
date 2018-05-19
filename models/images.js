7b4b9c8a157828a872db95c51ce81b8366be0d78
Sophia Butterworth <sophiabutterworth@gmail.com>
2018-05-19 10:45:26 -0400

    added images model file

---
+++ models/images.js
@@ -0,0 +1,19 @@
+module.exports = function (sequelize, DataTypes) {
+    var Image = sequelize.define("Image", {
+        url: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        alt: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    Image.associate = function (models) {
+        Image.belongsTo(models.Rental, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+};
