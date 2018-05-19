fcc01cbb14a2b2f71f3adc216b325d8aaff35129
Eliza Janus <emj4em@virginia.edu>
2018-05-19 11:20:17 -0400

    added a return statement to images.js and changed the bath datatype to integer in subletmodel file

--- models/subletModel.js
+++ models/subletModel.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
       description: {type: DataTypes.STRING, allowNull: false},
       rent: {type: DataTypes.INTEGER, allowNull: false},
       roommates: {type: DataTypes.INTEGER, allowNull: false},
-      baths: {type: DataTypes.DEC, allowNULL: false},
+      baths: {type: DataTypes.INTEGER, allowNULL: false},
       female: {type: DataTypes.BOOLEAN, allowNull: false},
       male: {type: DataTypes.BOOLEAN, allowNull: false},
       pets: {type: DataTypes.BOOLEAN, allowNull: false},
