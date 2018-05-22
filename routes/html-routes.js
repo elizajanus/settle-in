391f25351f1e07538d504ff607c17d3017fa4d42
Eliza Janus <emj4em@virginia.edu>
2018-05-22 11:53:27 -0400

    fixed homepage rendering on server

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,7 +13,7 @@ module.exports = function (app) {
 
 
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
 
